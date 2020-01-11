6988aebdd326cafaa172844330189d9ebcb3faa8
EddieOrozco <orozcoe230@gmail.com>
2020-01-11 17:34:58 -0600

    Created the POST and PATCH route and controller

--- app/controllers/api/cars_controller.rb
+++ app/controllers/api/cars_controller.rb
@@ -4,8 +4,30 @@ class Api::CarsController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    @car = Car.new(
+      seller_id: 2,
+      make: params[:make],
+      model: params[:model],
+      year: params[:year],
+      color: params[:color],
+    )
+    @car.save
+    render "show.json.jb"
+  end
+
   def show
     @car = Car.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @car = Car.find_by(id: params[:id])
+    @car.make = params[:make] || @car.make
+    @car.model = params[:model] || @car.model
+    @car.year = params[:year] || @car.year
+    @car.color = params[:color] || @car.color
+    @car.save
     render "show.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/cars" => "cars#index"
+    post "/cars" => "cars#create"
     get "/cars/:id" => "cars#show"
+    patch "/cars/:id" => "cars#update"
   end
 end
